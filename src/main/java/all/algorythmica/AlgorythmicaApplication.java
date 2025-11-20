731a6370b919632e58848bbd9ce0b1846b00ef58
Aleksey <vaznoe2024@mail.ru>
2025-11-20 09:14:50 +0300

    v.0 Создание проекта

---
+++ src/main/java/all/algorythmica/AlgorythmicaApplication.java
@@ -0,0 +1,13 @@
+package all.algorythmica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorythmicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorythmicaApplication.class, args);
+    }
+
+}
